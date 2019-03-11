ea57b7b04d16fe3623bab59eab44273c98dd831c
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2019-03-11 08:08:52 +0300

    Setup mocha

---
+++ api/tests/api-test.js
@@ -0,0 +1,20 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("../../app");
+
+const expect = chai.should();
+
+chai.use(chaiHttp);
+
+describe("Testing my api", () => {
+  it("testing that endpoint", (done) => {
+    chai
+      .request(app)
+      .get("/")
+      .set("accept", "application/json")
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+});
