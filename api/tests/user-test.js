f9c76088ddc60f281ad99ad8e9208481d9b3d541
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2019-03-13 01:18:07 +0300

    Add login endpoint

--- api/tests/user-test.js
+++ api/tests/user-test.js
@@ -8,22 +8,49 @@ chai.use(chaiHttp);
 
 const newUser = {
   id: 12,
-  email: "email.gmail.com",
+  email: "email@email.com",
   firstname: "Alela",
   lastname: "Syn",
-  password: "Password"
+  password: "password"
 };
 
 describe("User auth", () => {
-  it("user auth endpoint", done => {
+  it("correctly singups a user", done => {
     chai
       .request(app)
       .post("/user/auth/signup")
       .send(newUser)
       .set("accept", "application/json")
       .end((err, res) => {
-        console.log(res);
         res.should.have.status(201);
+        done();
+      });
+  });
+
+  it("correctly logins a user", done => {
+    chai
+      .request(app)
+      .post("/user/auth/login")
+      .send(newUser)
+      .set("accept", "application/json")
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+
+  it("rejects user with invalid credentials", done => {
+    const invalidCredentials = {
+      email: "email",
+      password: "pass"
+    };
+    chai
+      .request(app)
+      .post("/user/auth/login")
+      .send(invalidCredentials)
+      .set("accept", "application/json")
+      .end((err, res) => {
+        res.should.have.status(403);
         done();
       });
   });
